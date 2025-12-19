f6e4854fb2ec5492d0ace9a7126006da7f454a06
mohammadraziei <mohammadraziei1375@gmail.com>
2025-12-19 13:27:21 +0330

    feat: simplify Python classifiers and remove custom wheel class
    - Remove specific Python version classifiers in favor of generic "Programming Language :: Python :: 3"
    - Add OS classifiers for Linux, MacOS, and Windows to clarify cross-platform support
    - Remove custom BinaryWheel class from setup.py as it's no longer needed
    - Consolidate platform specification in setup.py to use the platforms parameter

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from setuptools import setup, find_namespace_packages
-from wheel.bdist_wheel import bdist_wheel
-
-
-class BinaryWheel(bdist_wheel):
-    def finalize_options(self):
-        super().finalize_options()
-        self.root_is_pure = False   
 
 if __name__ == "__main__":
     setup(
@@ -18,11 +11,5 @@ if __name__ == "__main__":
         options={
             "egg_info": {"egg_base": "."},
         },
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "Operating System :: POSIX :: Linux",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-        ],
         platforms=["Linux", "Windows", "macOS"],
     )
